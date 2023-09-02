67c30b0e3b7ca96745f48827f29bb326201164ec
Ivan Larsson <ivan@larssonrivera.se>
2023-09-02 15:17:50 +0200

    testing to change from nextjs <Image> component to regular html <img>

--- components/ImageWithText.js
+++ components/ImageWithText.js
@@ -1,28 +1,27 @@
 import React from "react";
 import styles from "./ImageWithText.module.css";
-import Image from "next/image";
 
 const ImageWithText = ({ imageUrl, title, paragraph, cta }) => {
   return (
     <div className={styles["image-with-text"]}>
-      <div className={styles["image-with-text__wrapper"]}> 
-      {imageUrl && (
-        <Image
-          src={imageUrl}
-          className={styles["image-with-text__image"]}
-          alt={title}
-          fill={true}
-        />
-      )}
-      <div className={styles["text-content"]}>
-        {title && <h2 className={styles.title}>{title}</h2>}
-        {paragraph && <p className={styles.paragraph}>{paragraph}</p>}
-        {cta && (
-          <a className={styles["cta-button"]} href={cta.href}>
-           {cta.text} 
-          </a>
+      <div className={styles["image-with-text__wrapper"]}>
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            className={styles["image-with-text__image"]}
+            alt={title}
+            fill={true}
+          />
         )}
-      </div>
+        <div className={styles["text-content"]}>
+          {title && <h2 className={styles.title}>{title}</h2>}
+          {paragraph && <p className={styles.paragraph}>{paragraph}</p>}
+          {cta && (
+            <a className={styles["cta-button"]} href={cta.href}>
+              {cta.text}
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
